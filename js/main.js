5aa97e810756df582a70fbcab85a76cd42a9ec96
LeMinhDuc <duclm96@gmail.com>
2017-01-15 18:47:45 +0700

    map and pop-up

--- js/main.js
+++ js/main.js
@@ -2,4 +2,18 @@ $(document).ready(function(){
   $('body').on('click', '.chat_button', function(){
     $("#chat_modal").modal("show");
   });
+
+  // $('body').scroll(function() {
+  //   $( ".map_container" ).append("<div></div>");
+  // });
+
+  $(function() {
+    $('img.trigger').hover(function() {
+      // $(event.target).parent().find('div.pop-up').css('opacity', 1);
+      $(event.target).parent().find('div.pop-up').show();
+    }, function() {
+      // $(event.target).parent().find('div.pop-up').css('opacity', 0);
+      $(event.target).parent().find('div.pop-up').hide();
+    });
+  });
 });
